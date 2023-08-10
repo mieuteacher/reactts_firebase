a44ea92eaf6fbbd06a2f3cab67c8fabc351ffc9e
Mieu Teacher <mieuteacher@gmail.com>
2023-08-10 15:47:30 +0700

    update load img uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import {uploadFileToStorage} from './firebase';
 
 function App() {
+  const [urlUpload, setUrlUpload] = useState <null | string | boolean>(null);
   return (
     <div className="App">
         <h1>Hello App</h1>
         <input type="file" onChange={async (e) => {
           if (e.target.files)  {
             let url = await uploadFileToStorage(e.target.files[0], "yourFiles")
-            console.log("url", url)
+            if(url) {
+              setUrlUpload(url)
+            }
           }
         }}/>
-
+        <img src={urlUpload ? `${urlUpload}` : ''} alt=''/>
     </div>
   );
 }
